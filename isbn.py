16585e339aaeb9055f88b87829dde45d0cf03ac5
Miki Ando <MikiAndo@Mikis-MacBook-Pro.local>
2019-09-17 20:17:38 +0700

    Create isbn.py

---
+++ isbn.py
@@ -0,0 +1,18 @@
+
+isbn = input("ISBN: ")
+
+while (isbn.isdigit() == False) or (len(isbn) != 10):
+    isbn = input("Retry: ")
+
+digit = int(isbn) % 10
+sumValue = 0
+
+for i in range (9,0,-1):
+    isbnDigit = ((int(isbn) // (10** (10 - i))) % 10) * i
+    sumValue = sumValue + isbnDigit
+
+if sumValue % 11 == digit:
+    print("YES")
+else:
+    print("NO")
+ 
